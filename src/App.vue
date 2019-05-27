8e92dd7d185401c659aab4570d844c7204207e4f
alexjmarshall <marshalexander@gmail.com>
2019-05-27 17:01:06 -0600

    working

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'app',
@@ -17,6 +17,7 @@ export default {
 </script>
 
 <style>
+@import "https://cdn.materialdesignicons.com/2.5.94/css/materialdesignicons.min.css";
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
